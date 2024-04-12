fb3e156b26506d22a541d53c7aa94866be31cddd
Carter958 <cartercastleberry8@gmail.com>
2024-04-12 12:44:34 -0600

    Deleted files

--- Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        MusicVisualizer visualizer = new MusicVisualizer("music.wav");
-        System.out.println(visualizer.toString());
-    }
-}
